bc8bb5b6054fae29447ca97ff12949c3a52b1e08
joehdodd <joehdodd@gmail.com>
2017-03-03 17:17:02 -0500

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+lightbox.option({
+
+    'maxWidth': 600,
+    'alwaysShowNavOnTouchDevices': false,
+    'wrapAround': true,
+    'showImageNumberLabel': false
+
+})
---
+++ js/search.js
@@ -0,0 +1,19 @@
+// Log key press in search field #photoSearch
+$("#photoSearch").keyup(function() {
+    // Store the searched value
+    var $searchText = $(this).val();
+    // Loop over each div in #gallery
+    $("#gallery div").each(function() {
+        // Target alt text and title text
+        var $altText = $(this).find("img").attr("alt");
+        var $titleText = $(this).find("img").attr("title");
+        // If search value matches alt and title, show image(s)
+        if($altText.toUpperCase().search($searchText.toUpperCase()) > -1 || $titleText.toUpperCase().search($searchText.toUpperCase()) > -1) {
+            // Show matches
+            $(this).show();
+            // Hide image(s) if no match
+        } else {
+            $(this).fadeOut(250);
+        }
+    });
+});
